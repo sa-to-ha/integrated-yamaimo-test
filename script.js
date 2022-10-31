bd4407fdc08a9a0534d1f2e31919befa6e9523ae
sa-to-ha <79373650+sa-to-ha@users.noreply.github.com>
2022-10-31 18:47:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let clickCount = 0
+let timer = null
 const heart = document.getElementById('heart')
 
 // クリック回数によって動作を分岐
@@ -8,11 +9,15 @@ heart.addEventListener('click', () => {
     if (window.matchMedia && window.matchMedia('(max-device-width: 479px)').matches) {  
         heart.style.width = 20 + clickCount * clickCount /1.5 + "%";
         if (clickCount === 1) {
-                clickCount = 0
+            timer = setTimeout(() => {heart.style.width ="20%";
+                    timer = null
+                    clickCount = 0
+            }, timeout)
         }
         if (20 + clickCount * clickCount /1.5 >= 70){
             if ( clickCount >= 110){
                 heart.style.width ="20%";
+                timer = null
                 clickCount = 0
             }else{
                 heart.style.width ="100%";
@@ -22,11 +27,15 @@ heart.addEventListener('click', () => {
       } else {
         heart.style.height = 10 + clickCount * clickCount /1.5 + "%";
         if (clickCount === 1) {
+            timer = setTimeout(() => { heart.style.height ="10%";
+            timer = null
             clickCount = 0
+            }, timeout)
         }
         if (10 + clickCount * clickCount /1.5 >= 70){
             if ( clickCount >= 120){
                 heart.style.height ="10%";
+                timer = null
                 clickCount = 0
             }else{
                 heart.style.height ="100%";
