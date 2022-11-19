9a9bb2c24aa4543d2e8b68a2c7316c0fe60f0e9e
sa-to-ha <79373650+sa-to-ha@users.noreply.github.com>
2022-11-19 16:08:43 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let clickCount = 0
 let timer = null
 const heart = document.getElementById('heart')
+const button = document.getElementById('button-container')
 
 // クリック回数によって動作を分岐
 heart.addEventListener('click', () => {
@@ -8,33 +9,35 @@ heart.addEventListener('click', () => {
     console.log(clickCount)
     if (window.matchMedia && window.matchMedia('(max-device-width: 479px)').matches) {  
         heart.style.width = 20 + clickCount * clickCount /1.5 + "%";
+        button.style.transform = "scale(" +  ( 1 - clickCount / 10 ) + ")";
         if (clickCount === 1) {
-            timer = setTimeout(() => {heart.style.width ="20%";
+            timer = setTimeout(() => {heart.style.width ="10.5%";
                     timer = null
                     clickCount = 0
             }, timeout)
         }
         if (20 + clickCount * clickCount /1.5 >= 70){
             if ( clickCount >= 110){
-                heart.style.width ="20%";
+                heart.style.width ="10.5%";
                 timer = null
                 clickCount = 0
+                button.style.transform = "scale(1)";
             }else{
                 heart.style.width ="100%";
                 clickCount = 150
             }
         }
       } else {
-        heart.style.height = 10 + clickCount * clickCount /1.5 + "%";
+        heart.style.height = 30 + clickCount * clickCount /1.5 + "%";
         if (clickCount === 1) {
-            timer = setTimeout(() => { heart.style.height ="10%";
+            timer = setTimeout(() => { heart.style.height ="5%";
             timer = null
             clickCount = 0
             }, timeout)
         }
         if (10 + clickCount * clickCount /1.5 >= 70){
             if ( clickCount >= 120){
-                heart.style.height ="10%";
+                heart.style.height ="5%";
                 timer = null
                 clickCount = 0
             }else{
